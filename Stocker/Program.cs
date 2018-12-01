ab165976368f084a922a7f3d7123b8c1113e4ba5
Bora Aydın <boraydin@hotmail.com>
2018-12-01 14:18:35 +0300

    Add project files.

---
+++ Stocker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
