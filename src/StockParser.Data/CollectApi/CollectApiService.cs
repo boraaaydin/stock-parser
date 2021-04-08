9d556bbcb20bcc417c100a02863f29504b9f55ef
Bora Aydın <boraaaydin@gmail.com>
2021-04-08 18:52:51 +0300

    CollectApiService added

---
+++ src/StockParser.Data/CollectApi/CollectApiService.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using StockParser.Data.CollectApi.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace StockParser.Data
+{
+    public class CollectApiService
+    {
+        private IHttpClientFactory _clientFactory;
+
+        public CollectApiService(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<CollectApiCurrency> GetCurrency()
+        {
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get,
+                    "https://api.collectapi.com/economy/currencyToAll?int=1&base=USD");
+                request.Headers.Add("Authorization", "apikey 0T0BMnqIoiOz1vzDDnMNEI:559hFvQ8HGp579mT5tPb8y");
+
+                var client = _clientFactory.CreateClient();
+                var response = await client.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString  = await response.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<CollectApiResponse<CollectApiCurrency>>(responseString);
+                    if (data.Success)
+                    {
+                        return data.Result;
+                    }
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
---
+++ src/StockParser.Data/CollectApi/Models/CollectApiResponse.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockParser.Data.CollectApi.Models
+{
+    public class CollectApiResponse<T>
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+        [JsonProperty("result")]
+        public T Result { get; set; }
+    }
+    public class CollectApiCurrency
+    {
+        [JsonProperty("base")]
+        public string Base { get; set; }
+        [JsonProperty("lastUpdate")]
+        public string LastUpdate { get; set; }
+        [JsonProperty("data")]
+        public List<CurrencyResponse> CurrencyList { get; set; }
+    }
+
+    public class CurrencyResponse
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("rate")]
+        public double Rate { get; set; }
+        [JsonProperty("calculatedstr")]
+        public double Calculatedstr { get; set; }
+        [JsonProperty("calculated")]
+        public double Calculated { get; set; }
+    }
+}
